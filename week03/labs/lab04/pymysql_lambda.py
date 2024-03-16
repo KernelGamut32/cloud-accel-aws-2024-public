27ff8bdfb6074eb6d5311bb93c5cca0877fbb6df
Allen Sanders <asanders@gamuttechnologysvcs.com>
2024-03-16 19:48:34 -0400

    added week 03 / lab 6

---
+++ week03/labs/lab04/pymysql_lambda.py
@@ -0,0 +1,19 @@
+import pymysql.cursors
+
+def lambda_handler(event, context):
+# Connect to the database
+  connection = pymysql.connect(host='<IP-OR-DB-CLUSTER-URL>',
+                               user='<username>',
+                               password='<secure-password>',
+                               db='widgets',
+                               charset='utf8mb4',
+                               cursorclass=pymysql.cursors.DictCursor)
+  
+  try:
+      with connection.cursor() as cursor:
+          sql = "select * from Catalog"
+          cursor.execute(sql)
+          result = cursor.fetchall()
+          print(result)
+  finally:
+      connection.close()
