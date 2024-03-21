8234e47749363cd261fa5a59f958430d1110a048
Allen Sanders <asanders@gamuttechnologysvcs.com>
2024-03-21 17:11:00 -0400

    updated week 4 / lab 2

--- week04/labs/lab01/queue-deploy/src/create-sqs-trigger.ts
+++ week04/labs/lab01/queue-deploy/src/create-sqs-trigger.ts
@@ -2,12 +2,11 @@ import { Construct } from "constructs";
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class CreateSqsTrigger extends Construct {
-  public trigger: lambda.CfnEventSourceMapping;
-
-  constructor(scope: Construct, id: string, eventSource: string, lambdaFunction: lambda.Function) {
+  constructor(scope: Construct, id: string, eventSource: string, 
+    lambdaFunction: lambda.Function) {
     super(scope, id);
 
-    this.trigger = new lambda.CfnEventSourceMapping(this, id, {
+    new lambda.CfnEventSourceMapping(this, id, {
         batchSize: 10,
         eventSourceArn: eventSource,
         functionName: lambdaFunction.functionName,
