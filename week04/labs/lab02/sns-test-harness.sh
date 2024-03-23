470cd387b67aa9e6a4b0197742309e3b28298028
Allen Sanders <asanders@gamuttechnologysvcs.com>
2024-03-23 16:41:21 -0400

    fixed test harness

--- week04/labs/lab02/sns-test-harness.sh
+++ week04/labs/lab02/sns-test-harness.sh
@@ -2,20 +2,20 @@
 
 # Publish regular EU order message
 aws sns publish \
-    --message "{"location": "eu-west", "quantity": 34}" \
+    --message '{"location": "eu-west", "quantity": 34}' \
     --topic-arn $1
 
 # Publish large EU order message
 aws sns publish \
-    --message "{"location": "eu-north", "quantity": 111}" \
+    --message '{"location": "eu-north", "quantity": 111}' \
     --topic-arn $1
 
 # Publish large other order message
 aws sns publish \
-    --message "{"location": "us-west", "quantity": 222, "state": "OH"}" \
+    --message '{"location": "us-west", "quantity": 222, "state": "OH"}' \
     --topic-arn $1
 
 # Publish regular other order message
 aws sns publish \
-    --message "{"location": "us-east", "quantity": 33, "state": "NY"}" \
+    --message '{"location": "us-east", "quantity": 33, "state": "NY"}' \
     --topic-arn $1
