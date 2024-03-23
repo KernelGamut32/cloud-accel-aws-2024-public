a97d93a201389e169adcba7c3124bf83815e8e15
Allen Sanders <asanders@gamuttechnologysvcs.com>
2024-03-23 14:11:32 -0400

    added sns test harness

---
+++ week04/labs/lab02/sns-test-harness.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Publish regular EU order message
+aws sns publish \
+    --message "{"location": "eu-west", "quantity": 34}" \
+    --topic-arn $1
+
+# Publish large EU order message
+aws sns publish \
+    --message "{"location": "eu-north", "quantity": 111}" \
+    --topic-arn $1
+
+# Publish large other order message
+aws sns publish \
+    --message "{"location": "us-west", "quantity": 222, "state": "OH"}" \
+    --topic-arn $1
+
+# Publish regular other order message
+aws sns publish \
+    --message "{"location": "us-east", "quantity": 33, "state": "NY"}" \
+    --topic-arn $1
