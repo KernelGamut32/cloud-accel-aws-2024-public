e5f20c374147d4678bf0653bcd5d2a22a5266ee1
Allen Sanders <asanders@gamuttechnologysvcs.com>
2024-08-10 16:30:19 -0400

    updated cloud9 script

--- cloud9.sh
+++ cloud9.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 aws cloud9 create-environment-ec2 --name $1 --instance-type $2 --image-id $3
 sleep 60
+instance_id=$(aws ec2 describe-instances --query "Reservations[].Instances[] | [? Tags[?Value.contains(@, '$1')]].InstanceId" --output text)
 volume_id=$(aws ec2 describe-instances --query "Reservations[].Instances[] | [? Tags[?Value.contains(@, '$1')]].BlockDeviceMappings[0].Ebs.VolumeId" --output text)
 aws ec2 modify-volume --volume-id $volume_id --size 50 > /dev/null
+aws ec2 reboot-instances --instance-ids $instance_id
